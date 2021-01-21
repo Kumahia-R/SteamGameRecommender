2f631f577d03c756dd0a098ff5738477e85ad317
Richard Kumahia <Rkumahia@Gmail.com>
2021-01-21 12:50:39 -0500

    Added comments and made some more touches to steamUser.py. Will most likely focus on main.py from this point

--- SteamUser.py
+++ SteamUser.py
@@ -1,54 +1,28 @@
 import requests
-import urllib3
 from bs4 import BeautifulSoup
 
-API_KEY = 'XXXXXXXXXXXXXXXXXXXX'
-GetFriendsURL = 'https://api.steampowered.com/ISteamUser/GetFriendList/v1/'
-GotOwnedURL = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/'
-GetPlayerSummariesURL = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/'
+API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'  # key to access any of the steam APIs
+# Following URLs are the Steam API URLs
+GetFriendsURL = 'https://api.steampowered.com/ISteamUser/GetFriendList/v1/'  # Returns user's friend list and their information, we're specifically looking for their steamids
+GotOwnedURL = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/'  # Returns user's owned games
+GetPlayerSummariesURL = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/'  # Returns a myriad of the user's information but we're only using this to get the user's profile name
 
 
-class Game:  # Game class, will have all the necessary info for a game so it can be readily accessed
-    def __init__(self, app_id, name, play_time):
-        self.app_id = app_id
-        self.name = name
-        self.play_time = play_time
-
-    def get_store_page(self):
-        store_page_url = "https://store.steampowered.com/app/" + str(self.app_id) + "/" + self.name + "/"
-        return store_page_url
-
-    def get_game_tags(self):
-        page = self.get_store_page()
-        r = requests.get(page).text
-        soup = BeautifulSoup(r, 'html.parser')
-        tag_html = soup.find("div", attrs={"class":"glance_tags popular_tags"})
-        game_tags = tag_html.find_all("a")
-        tag_list = []
-        for i in game_tags:
-            tag_list.append(i.text.replace('\n', ' ').strip())
-        return tag_list
-
-
-    def __eq__(self, other): #compares the appids of 2 Game objects
-        #appid is the only completely unique variable in the object so it'll be the one compared
-        self_id = self.app_id
-        other_id = other.app_id
-        return self_id == other_id
-
-    def __hash__(self):
-        return hash(self.app_id)
-
-
-class SteamUser:  # user class
-    # will probably create a "Game" class to make future changes easier (maybe a function to account for time played, etc)
-    def __init__(self, userID):  # constructor accesses user data such as list of games played and friends
+class SteamUser:
+    # class based on the Steam User
+    # contains any information I may need for this project, such as their profile name and friend list
+    def __init__(self, userID):
+        # constructor accesses user data such as list of games played and friends
         self.userID = userID
+        # using the Steam APIs to get the user's information
         self.user_summary = requests.get(GetPlayerSummariesURL, params={'key': API_KEY, 'steamids': userID}).json()
         self.friendList = requests.get(GetFriendsURL, params={'key': API_KEY, 'steamid': int(userID)}).json()
-        self.user_list = requests.get(GotOwnedURL, params={'key': API_KEY, 'steamid': int(userID), 'include_appinfo': True, 'include_played_free_games': True}).json()
+        self.user_list = requests.get(GotOwnedURL,
+                                      params={'key': API_KEY, 'steamid': int(userID), 'include_appinfo': True,
+                                              'include_played_free_games': True}).json()
 
-    def get_user_library(self):  # returns the names of the user's games in a list
+    def get_user_library(self):
+        # returns the names of the user's games in a set
         game_list = set()
         for i in self.user_list["response"]["games"]:
             new_game = Game(i['appid'], i['name'], i['playtime_forever'])
@@ -56,18 +30,66 @@ class SteamUser:  # user class
         return game_list
 
     def get_profile_name(self):
+        # returns the profile name of the user (will make displaying it easier)
         profile_name = self.user_summary['response']['players'][0]['personaname']
         return profile_name
 
     def get_friends_list(self):
-        friend_names = set()
+        # Returns the user's friend list
+        friend_names = []
         for i in self.friendList["friendslist"]["friends"]:
+            # this loop will make a new SteamUser object with each friend's steamid being the argument
+            # then will add the new SteamUser to the list
             new_friend = SteamUser(i['steamid'])
-            friend_names.add(new_friend)
+            friend_names.append(new_friend)
         return friend_names
 
     def print_friend_list(self):
+        # prints the names of the user's friends
         print("This is your friend list:")
-        friends_print = self.get_friends_list()
+        friend_index = 0  # index to access one friend per loop iteration
+        friends_print = self.get_friends_list()  # gets the list of friends as SteamUsers
         for i in friends_print:
-            print(i.get_profile_name())
+            # since the friends are already listed as SteamUsers
+            # we just need to get their names using get_profile_name()
+            print(f'{friend_index + 1}. {i.get_profile_name()}')
+            friend_index += 1
+
+
+class Game:
+    # Game class, will have all the necessary info for a game so it can be readily accessed
+    def __init__(self, app_id, name, play_time):
+        self.app_id = app_id  # Every Steam game has a unique ID
+        self.name = name  # The title of the game, not unique
+        self.play_time = play_time  # amount of time the user has played the game
+
+    def get_store_page(self):
+        # method to get the url of the game's store page
+        store_page_url_base = "https://store.steampowered.com/app/" # URL base
+        store_page_url = store_page_url_base + str(self.app_id) + "/" + self.name + "/"  # gets the rest of the page URL
+        return store_page_url
+
+    def get_game_tags(self):
+        # method to get the tags(genres) associated with the steam game
+        page_url = self.get_store_page()
+        page_html_text = requests.get(page_url).text  # gets the Steam page's HTML as text
+        soup = BeautifulSoup(page_html_text, 'html.parser')  # turns the HTML into an object that makes parsing through it easier
+        tag_html = soup.find("div", attrs={"class": "glance_tags popular_tags"})  # gets the section of the HTML that contains the game's tags
+        if tag_html == None:
+            return
+        game_tags = tag_html.find_all("a")  # further filters the HTML to make getting only the tags easier
+        tag_list = []
+        for i in game_tags:
+            # adds the game's tags as an str to the list tag_list
+            tag_list.append(i.text.replace('\n', ' ').strip())  # there's lots of whitespace around the actual tage name so this is to exclude it
+        return tag_list
+
+    def __eq__(self, other):  # compares the appids of 2 Game objects
+        # appid is the only completely unique variable in the object so it'll be the one compared
+        self_id = self.app_id
+        other_id = other.app_id
+        return self_id == other_id
+
+    def __hash__(self):
+        # had to overload the hashing method to only hash the appid int (causes an error otherwise)
+        return hash(self.app_id)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
-from steamUser import SteamUser
-from steamUser import Game
-#my_id = '76561198415661881'
-#me = SteamUser(my_id)
-#my_url = 'https://store.steampowered.com/wishlist/profiles/76561198415661881/#sort=order'
+from SteamUser import SteamUser
+from SteamUser import Game
+
+
 # function to get the user's steamid based on a url they input
 def get_steam_id(url):
     id_start = url.find("profiles/") + 9
@@ -11,19 +10,24 @@ def get_steam_id(url):
     steam_id = url[id_start:id_end]
     return steam_id
 
-#this function will return a set of games from the friend's list that the user won't have
+
+# this function will return a set of games from the friend's list that the user won't have
 def compare_libraries(user, friend):
     user_library = user.get_user_library()
     friend_library = friend.get_user_library()
     unique_games = friend_library.difference(user_library)
     return unique_games
 
+
 user_url = input("Enter any URL involving your steam profile")
 user_id = get_steam_id(user_url)
 steam_user = SteamUser(user_id)
-steam_friend = SteamUser('76561198121248324')
+steam_user.print_friend_list()
+friend_index = input("Choose which friend you want to compare your library to (Enter the corresponding number):")
+steam_friend = steam_user.get_friends_list()[int(friend_index) - 1]
 different_games = compare_libraries(steam_user, steam_friend)
+index = 0
 for i in different_games:
     i.get_game_tags()
-print(f'Your Steam ID is {user_id}')
-steam_user.print_friend_list()
+    print(index)
+    index += 1
